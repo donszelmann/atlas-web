9f4989ba13682b4576b011ddcf637e452af92645
Mark Donszelmann <Mark.Donszelmann@cern.ch>
2010-03-31 13:39:10 +0200

    Reload on resize

--- htdocs/atlas-point1/page1/index.php
+++ htdocs/atlas-point1/page1/index.php
@@ -33,7 +33,7 @@
 <script type="text/javascript" src="../script-new/jquery-1.2.1.min.js"></script>
 <script type="text/javascript" src="../script-new/menu.js"></script>
 </head>
-<body>
+<body onResize="window.location.href = window.location.href;">
 <div id="wrapper">
     <div id="topRow">
         <div id="login" class="container">
